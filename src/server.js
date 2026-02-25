c0a3f435852dfa08c34b5ce4bc6e2423178c3940
eMlock11 <hyuke0309@gmail.com>
2026-02-24 21:30:58 -0300

    matheus desprovido de intelecto

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import 'dotenv/config'; //chaves
 import express from 'express'; // frameworks
 import cors from 'cors'; // ele vai permitir a comunicação com o frontend
 import companyRouter from './routes/company'
-
+import userRouter from './routes/user'
 
 const app = express(); // estou criando um app
 app.use(cors()); // aqui falo qual cors
 app.use(express.json()); // aqui falo que vai usar o formato json
 
 app.use('/company', companyRouter);
+app.use('/user', userRoarter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`HTTP => http://localhost:${PORT}`));
