e594340ef3f8512a52f43f1c3aa9919765609df3
Kevin-Dorinho <kevindorinhO@hotmail.com>
2026-03-03 21:08:09 -0300

    colocando GET pra encontrar um ID

--- src/services/company.js
+++ src/services/company.js
@@ -8,7 +8,13 @@ export async function createCompany(req, res, _next){
     return res.status(201).json(c);
 }
 
-export async function readCompany(rec, res, _next) {
+export async function readCompany(req, res, _next) {
     let companies = await prisma.company.findMany()
     return res.status(200).json(companies);
+}
+
+export async function showCompany(req, res, _next) {
+    let id = Number(req.params.id);
+    let c = await prisma.company.findFirst({where: {id: id} });
+    return res.status(200).json(c);
 }
