c23df8b77e1da52b836e8c699739b7df76064358
IceHatsuke <gabriele16022005@gmail.com>
2026-03-10 21:29:52 -0300

    criando editPayment

--- src/services/payment.js
+++ src/services/payment.js
@@ -26,10 +26,30 @@ export async function readPayment(req, res, _next) {
     return res.status(200).json(payments);
 }
 
-    
-
 export async function showPayment(req, res, _next) {
     let id = Number(req.params.id);
     let p = await prisma.payment.findFirst({where: {id:id} });
     return res.status(200).json(p);
+}
+
+export async function editPayment(req, res, _next) {
+    let id = Number(req.params.id);
+    const {to_date, due_date, value, paymentForm, advertising, key, type} = req.body
+
+    let p = await prisma.payment.findFirst({where: {id:id} })
+
+    if(!p){
+        return res.status(404).json("Não encontrei "+ id);
+    }
+
+    if(to_date) p.to_date = to_date;
+    if(due_date) p.due_date = to_date;
+    if(value) p.value = value;
+    if(paymentForm) p.paymentForm = paymentForm;
+    if(advertising) p.advertising = advertising;
+    if(key) p.key = key;
+    if(type) p.type = type;
+
+    await p.save();
+    return res.status(202).json(p);
 }
