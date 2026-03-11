145c5e9dd9e231d2f149471bb48fb2af3e6609d6
Kevin-Dorinho <kevindorinhO@hotmail.com>
2026-03-10 21:24:33 -0300

    criamos o PUT do 0

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createCompany, readCompany, showCompany } from '../services/company.js';
+import { createCompany, readCompany, showCompany, editCompany } from '../services/company.js';
 
 
 const router = Router();
 
 router.post('/', createCompany);
 router.get('/', readCompany);
-router.get('/:id', showCompany)
+router.get('/:id', showCompany);
+router.put('/:id', editCompany)
 
 export default router;
--- src/services/company.js
+++ src/services/company.js
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { z } from 'zod';
+import { id } from "zod/locales";
+import { attachSave } from "../utils/save.js";
+
 const prisma = new PrismaClient();
 
 export async function createCompany(req, res, _next){
@@ -10,20 +13,43 @@ export async function createCompany(req, res, _next){
 
 export async function readCompany(req, res, _next) {
     
-    const {name, address, category } = req.query
+    const {name, places, category } = req.query
 
     let consult = {}
 
     if (name) consult.name = {contains: "%"+name+"%"}
-    if (address) consult.address = {contains: "%"+address+"%"}
+    if (places) consult.places = {contains: "%"+address+"%"}
     if (category) consult.category = {contains: "%"+category+"%"}
 
     let companies = await prisma.company.findMany({where: consult})
     return res.status(200).json(companies);
 }
 
+export async function editCompany(req, res, _next) {
+
+    const {name, places, category, cnpj } = req.body
+    let id = Number(req.params.id);
+
+    if(!u){
+        return res.status(404).json("Não econtrei " + id);
+    }
+
+    u = attachSave(u, 'user');
+    
+    if (name) c.name = name
+    if (places) c.places = places
+    if (category) c.category = category
+    if (cnpj) c.cnpj = cnpj
+
+    c.save();
+
+    let c = await prisma.company.findFirst({where: {id: id}})
+    return res.status(200).json(companies);
+
+}
+
 export async function showCompany(req, res, _next) {
     let id = Number(req.params.id);
     let c = await prisma.company.findFirst({where: {id: id} });
-    return res.status(200).json(c);
+    return res.status(202).json(c);
 }
